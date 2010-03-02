86e943152737370945ff4d7f7a934b4ee474982f
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-03-02 19:22:40 +0100

    Bump to 0.97 ^.^"

--- atarashii/usr/share/pyshared/atarashii/__init__.py
+++ atarashii/usr/share/pyshared/atarashii/__init__.py
@@ -1,4 +1,4 @@
-#  Version 0.95
+#  Version 0.97
 #  Copyright (C) 2010 Ivo Wetzel
 #  <http://github.com/BonsaiDen/Atarashii/>
 #
@@ -21,7 +21,7 @@
 """
 Atarashii Twitter Client
 """
-__version__ = '0.96'
+__version__ = '0.97'
 __author__ = 'Ivo Wetzel <ivo.wetzel@googlemail.com>'
 __license__ = 'GPL3'
 
