a7c65e31ce2a53061221c0d97a0994ab5e9c748f
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-03-02 19:21:30 +0100

    Added two commentlines for more readability.

--- atarashii/usr/share/pyshared/atarashii/updater.py
+++ atarashii/usr/share/pyshared/atarashii/updater.py
@@ -116,6 +116,7 @@ class Updater(threading.Thread):
 		gobject.idle_add(lambda: self.main.gui.checkRead())
 	
 	
+	# Load the initial tweets --------------------------------------------------
 	def getInitTweets(self):
 		updates = []
 		try:
@@ -138,6 +139,7 @@ class Updater(threading.Thread):
 		self.tweetsLoaded = 1
 		return True
 	
+	# Load the initial messages ------------------------------------------------
 	def getInitMessages(self):
 		messages = []
 		try:
