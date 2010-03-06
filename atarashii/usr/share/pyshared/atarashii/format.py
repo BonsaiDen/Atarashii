ac08074f30ef3684fbf765877100695c98ad6e9b
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-03-06 14:31:40 +0100

    Again... regex makes kittens cry...

--- atarashii/usr/share/pyshared/atarashii/format.py
+++ atarashii/usr/share/pyshared/atarashii/format.py
@@ -18,8 +18,8 @@
 # ------------------------------------------------------------------------------
 import re, urllib
 urlRegex = re.compile("((mailto\:|(news|(ht|f)tp(s?))\://){1}[^\s\)\]]+)")
-atRegex = re.compile("@((){1}[^\?\s\:]+)")
-tagRegex = re.compile("\#((){1}[^\?\s\+\-\:]+)") #re.compile("\#((){1}\S+)")
+atRegex = re.compile("@((){1}[^\?\s\:\!]+)")
+tagRegex = re.compile("\#((){1}[^\?\s\+\-\:\!]+)") #re.compile("\#((){1}\S+)")
 
 from lang import lang
 
