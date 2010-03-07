b04120b715fa6a1de388823690e83235ec7b157e
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-03-07 18:32:12 +0100

    The kittens are pushing to 0.99.3!

--- atarashii/usr/share/pyshared/atarashii/__init__.py
+++ atarashii/usr/share/pyshared/atarashii/__init__.py
@@ -16,7 +16,7 @@
 """
 Atarashii Twitter Client
 """
-__version__ = '0.99.2'
+__version__ = '0.99.3'
 __author__ = 'Ivo Wetzel <ivo.wetzel@googlemail.com>'
 __copyright__ = 'Copyright (c) 2010 Ivo Wetzel'
 __license__ = 'GPL 3'
