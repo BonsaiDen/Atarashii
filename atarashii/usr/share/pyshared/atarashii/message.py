c6af4c5004fe407f9c5458422b0b7f44629708c6
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-03-08 19:40:02 +0100

    Rename some stuff for more consistency.

--- atarashii/usr/share/pyshared/atarashii/message.py
+++ atarashii/usr/share/pyshared/atarashii/message.py
@@ -62,10 +62,10 @@ class HTML(view.HTMLView):
 		# Do the rendering!
 		self.count = 0
 		for num, obj in enumerate(self.items):
-			tweet, img, mode = obj
+			item, img, mode = obj
 			
 			# Fix some stuff for the seperation of continous new/old items
-			newTimeline = tweet.id > self.initID
+			newTimeline = item.id > self.initID
 			if newTimeline:
 				self.count += 1
 			
@@ -76,23 +76,23 @@ class HTML(view.HTMLView):
 				newest = True
 			
 			# Create Tweet HTML
-			text = self.formatter.parse(tweet.text)
-			highlight = tweet.recipient_screen_name != self.main.username
+			text = self.formatter.parse(item.text)
+			highlight = item.recipient_screen_name != self.main.username
 			
 			# Spacer
 			if num > 0:
 				spacer = ""
-				if lastname != tweet.sender.screen_name or newTimeline:
+				if lastname != item.sender.screen_name or newTimeline:
 					if lastHighlight != highlight:
 						spacer = ""
 					
-					elif tweet.id > self.initID:
+					elif item.id > self.initID:
 						spacer = "1"
 				
 				elif highlight != lastHighlight:
-					spacer = "3" if tweet.id > self.initID else ""
+					spacer = "3" if item.id > self.initID else ""
 				
-				elif tweet.id > self.initID:
+				elif item.id > self.initID:
 					spacer = "4" if highlight else "6"
 				
 				elif highlight:
@@ -103,7 +103,7 @@ class HTML(view.HTMLView):
 				
 				renderitems.insert(0, '<div class="spacer%s"></div>' % spacer)
 			
-			lastname = tweet.sender.screen_name
+			lastname = item.sender.screen_name
 			lastHighlight = highlight
 			
 			
@@ -123,9 +123,9 @@ class HTML(view.HTMLView):
 				newestAvatar = True
 			
 			if (num < len(self.items) - 1 and \
-				(tweet.sender.screen_name != \
+				(item.sender.screen_name != \
 				self.items[num + 1][0].sender.screen_name or \
-				tweet.recipient_screen_name != \
+				item.recipient_screen_name != \
 				self.items[num + 1][0].recipient_screen_name or newAvatar \
 				)) or num == len(self.items) - 1 or newTimeline:
 				
@@ -133,29 +133,29 @@ class HTML(view.HTMLView):
 							<img width="32" src="file://%s" title="''' + \
 							lang.htmlInfo + '''"/></a>'''
 				
-				avatar = avatar % (tweet.sender.screen_name, img, 
-									tweet.sender.name, 
-									tweet.sender.followers_count, 
-									tweet.sender.friends_count, 
-									tweet.sender.statuses_count)
+				avatar = avatar % (item.sender.screen_name, img, 
+									item.sender.name, 
+									item.sender.followers_count, 
+									item.sender.friends_count, 
+									item.sender.statuses_count)
 			
 			else:
 				avatar = ""
 			
 			# Class
-			cls = 'oldtweet' if tweet.id <= self.initID else 'tweet'
+			cls = 'oldtweet' if item.id <= self.initID else 'tweet'
 
 			# HTML
-			if tweet.recipient_screen_name != self.main.username:
+			if item.recipient_screen_name != self.main.username:
 				mode = lang.messageTo
-				name = tweet.recipient_screen_name
+				name = item.recipient_screen_name
 				reply = "display: none;"
 			
 			else:
 				mode = lang.messageFrom
-				name = tweet.sender.screen_name
+				name = item.sender.screen_name
 				reply = ""
-				cls = "highlightold" if tweet.id <= self.initID else "highlight"
+				cls = "highlightold" if item.id <= self.initID else "highlight"
 			
 			# HTML Snippet
 			html = '''
@@ -167,7 +167,7 @@ class HTML(view.HTMLView):
 			<div class="actions">
 				<div class="doretweet" style="''' + reply + \
 				'''"><a href="message:%s:%d:%d" title="''' + \
-					(lang.htmlReply % tweet.sender.screen_name) + '''"></a>
+					(lang.htmlReply % item.sender.screen_name) + '''"></a>
 				</div>
 			</div>
 			
@@ -180,7 +180,7 @@ class HTML(view.HTMLView):
 				</div>
 				<div class="time">
 					<a href="http://twitter.com/%s/statuses/%d" title="''' + \
-					(self.absolute_time(tweet.created_at)) + '''">%s</a>
+					(self.absolute_time(item.created_at)) + '''">%s</a>
 				</div>
 			</div>
 			</div>'''
@@ -191,20 +191,20 @@ class HTML(view.HTMLView):
 					avatar,
 					
 					# Actions
-					tweet.sender.screen_name, tweet.sender.id, num, 	
+					item.sender.screen_name, item.sender.id, num, 	
 					
 					# Text
-					tweet.sender.screen_name, 
-					tweet.sender.name.strip(), 
+					item.sender.screen_name, 
+					item.sender.name.strip(), 
 					name, 
 					text, 	
 					
 					# Time
-					tweet.sender.screen_name,
-					tweet.id,
-					self.relative_time(tweet.created_at))
+					item.sender.screen_name,
+					item.id,
+					self.relative_time(item.created_at))
 			
-			if tweet.id == self.newestID:
+			if item.id == self.newestID:
 				html = '</div>' + html
 			
 			self.main.gui.setTitle()
