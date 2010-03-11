5eb8a4b91ffa71218a5abb6050ca3ec568fb2ab3
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-03-11 19:19:19 +0100

    New file for constants. The contents of this file will be * imported so make sure there's nothing in here that could break something.

---
+++ atarashii/usr/share/pyshared/atarashii/constants.py
@@ -0,0 +1,41 @@
+#  This file is part of Atarashii.
+#
+#  Atarashii is free software: you can redistribute it and/or 
+#  modify it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Atarashii is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License along with
+#  Atarashii. If not, see <http://www.gnu.org/licenses/>.
+
+
+# Constants --------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
+# Rewteets
+RETWEET_ASK = 0			# Can be anything
+RETWEET_NEW = 1			# Can be anything
+RETWEET_OLD = 2			# Can be anything
+
+# General Stuff
+UNSET_ID_NUM = -1		# Must be lower than 0
+UNSET_TEXT = ""			# Must be ""
+UNSET_LABEL = ""		# Should be ""
+UNSET_TIMEOUT = -1		# Don't know if this is needed anymore at all...
+
+# Modes
+MODE_TWEETS = 2			# Can be anything
+MODE_MESSAGES = 0		# Can be anything
+MODE_SEARCH = 4			# Can be anything, currently unused
+
+# HTML
+HTML_UNSET_ID = -1		# Must be lower than 0
+HTML_RESET = -1			# Can be anything
+HTML_LOADING = 0		# Can be anything
+HTML_LOADED = 1			# Can be anything
+
