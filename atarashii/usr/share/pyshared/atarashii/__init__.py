a05450180bc99721f73aefafbccfada34321109a
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-03-16 06:00:25 +0100

    Update to 0.99.9(.999999999999...) - Contextual Delete Edition!

--- atarashii/usr/share/pyshared/atarashii/__init__.py
+++ atarashii/usr/share/pyshared/atarashii/__init__.py
@@ -16,7 +16,7 @@
 """
 Atarashii Twitter Client
 """
-__version__ = '0.99.8b'
+__version__ = '0.99.9'
 __author__ = 'Ivo Wetzel <ivo.wetzel@googlemail.com>'
 __copyright__ = 'Copyright (c) 2010 Ivo Wetzel'
 __license__ = 'GPL 3'
