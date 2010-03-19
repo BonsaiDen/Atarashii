7a1f94d6b2e43fbec239597cef354468c88d0720
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-03-19 23:34:23 +0100

    Update to Version 0.99.14 - One Button to rule them all! - Edition

--- atarashii/usr/share/pyshared/atarashii/__init__.py
+++ atarashii/usr/share/pyshared/atarashii/__init__.py
@@ -16,7 +16,7 @@
 """
 Atarashii Twitter Client
 """
-__version__ = '0.99.13a'
+__version__ = '0.99.14'
 __author__ = 'Ivo Wetzel <ivo.wetzel@googlemail.com>'
 __copyright__ = 'Copyright (c) 2010 Ivo Wetzel'
 __license__ = 'GPL 3'
