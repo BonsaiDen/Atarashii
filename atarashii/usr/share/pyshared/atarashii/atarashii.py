0c170c53bdf8af19380edbc08925f2add2286c71
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-03-19 07:02:23 +0100

    Remove todo.

--- atarashii/usr/share/pyshared/atarashii/atarashii.py
+++ atarashii/usr/share/pyshared/atarashii/atarashii.py
@@ -13,8 +13,6 @@
 #  You should have received a copy of the GNU General Public License along with
 #  Atarashii. If not, see <http://www.gnu.org/licenses/>.
 
-# TODO add real errors to the tray icon tooltip
-
 
 # DBUS Integration -------------------------------------------------------------
 # ------------------------------------------------------------------------------
