1307df2024e77742ad1e7cd5bb6b4ad44c8b94fb
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-03-22 09:54:21 +0100

    Fix check for jpeg extension.

--- atarashii/usr/share/pyshared/atarashii/settings.py
+++ atarashii/usr/share/pyshared/atarashii/settings.py
@@ -228,8 +228,9 @@ class Settings:
         for i in os.listdir(ATARASHII_DIR):
             cache_file = os.path.join(ATARASHII_DIR, i)
             
+            # TODO determin wether to check for last access or last modification
             if time.time() - os.stat(cache_file).st_atime > CACHE_TIMEOUT:
-                if cache_file[-4:].lower() in ('.jpg', '.png', 'gif'):
+                if cache_file[-4:].lower() in ('jpeg', '.jpg', '.png', 'gif'):
                     try:
                         os.unlink(cache_file)
                     
