9acf1be53f4910745c95128ab47701679e0debc2
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-03-24 13:41:43 +0100

    Increase the version on the next branch.

--- atarashii/usr/share/pyshared/atarashii/__init__.py
+++ atarashii/usr/share/pyshared/atarashii/__init__.py
@@ -16,7 +16,7 @@
 """
 Atarashii Twitter Client
 """
-__version__ = '0.99.16'
+__version__ = '0.99.16a'
 __author__ = 'Ivo Wetzel <ivo.wetzel@googlemail.com>'
 __copyright__ = 'Copyright (c) 2010 Ivo Wetzel'
 __license__ = 'GPL 3'
