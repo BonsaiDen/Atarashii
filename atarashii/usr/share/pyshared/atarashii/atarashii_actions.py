de4f8bf14f368cd751a6936c7755164455ca0a79
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-03-24 20:55:57 +0100

    Save the normalized position.

--- atarashii/usr/share/pyshared/atarashii/atarashii_actions.py
+++ atarashii/usr/share/pyshared/atarashii/atarashii_actions.py
@@ -108,9 +108,8 @@ class AtarashiiActions:
         
         self.gui.html.save_first()
         self.gui.message.save_first()
-        if self.gui.tray.on_screen(): # Don't save off screen positions
-            self.settings['position'] = str(self.gui.get_position())
         
+        self.settings['position'] = str(self.gui.get_normalized_position())
         size = self.gui.get_allocation()
         self.settings['size'] = str((size[2], size[3]))
         self.settings['username'] = self.username
