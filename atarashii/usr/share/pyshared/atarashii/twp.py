48d728fa5a1c217c1cb16027718a27e7679b588a
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-04-01 13:45:26 +0200

    Make this part of Atarashii.

--- atarashii/usr/share/pyshared/atarashii/twp.py
+++ atarashii/usr/share/pyshared/atarashii/twp.py
@@ -1,19 +1,17 @@
-#  This file is part of twp.
+#  This file is part of Atarashii.
 #
-#  twp is free software: you can redistribute it and/or
+#  Atarashii is free software: you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  twp is distributed in the hope that it will be useful,
+#  Atarashii is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License along with
-#  twp. If not, see <http://www.gnu.org/licenses/>.
-
-# TODO create a setup.py
+#  Atarashii. If not, see <http://www.gnu.org/licenses/>.
 
 
 # Tweet Parser and Formatter ---------------------------------------------------
