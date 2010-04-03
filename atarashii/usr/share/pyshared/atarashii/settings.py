386eb91dac16dadf6ad64d61cda0dd16548e8f68
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-04-03 13:20:45 +0200

    Use modifictation time instead of last access.

--- atarashii/usr/share/pyshared/atarashii/settings.py
+++ atarashii/usr/share/pyshared/atarashii/settings.py
@@ -240,8 +240,8 @@ class Settings:
     def check_cache(self):
         for i in os.listdir(ATARASHII_DIR):
             cache_file = os.path.join(ATARASHII_DIR, i)
-            if time.time() - os.stat(cache_file).st_atime > CACHE_TIMEOUT:
-                if cache_file[-4:].lower() in ('jpeg', '.jpg', '.png', 'gif'):
+            if cache_file[-4:].lower() in ('jpeg', '.jpg', '.png', 'gif'):
+                if int(time.time() - os.stat(cache_file).st_mtime) > CACHE_TIMEOUT:
                     try:
                         os.unlink(cache_file)
                     
