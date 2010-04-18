8e19cdf47c95282911936780ce8c7e21fd4ae732
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-04-18 21:23:10 +0200

    More color stuff. Also added comments to themes.py.

--- atarashii/usr/share/pyshared/atarashii/themes.py
+++ atarashii/usr/share/pyshared/atarashii/themes.py
@@ -22,32 +22,77 @@ COLOR_THEMES = {
         'title_en_US': 'Fuji blue(default)',
         
         'colors':  {
+            # Background color of the loading screens
             'BACKGROUND': 'dbdbdb',
             
+            # Color of new tweets and spacers between them
+            # (if from the same user)
             'TWEET': 'd8e4ed',
+            
+            # Color of old tweets and spacers between them
+            # (if from the same user)
             'TWEET_OLD': 'ffffff',
             
+            # Spacer between two old tweets from different users
             'SPACER_TWEET_OLD': 'd9d9d9',
+            
+            # Spacer between two new tweets from different users
             'SPACER_TWEET': 'beced0',
+            
+            # Spacer between a tweet and a new highlighted one
             'SPACER_TWEET_HIGHLIGHT': 'bed1d8',
+            
+            # Spacer between a tweet and a old highlighted one
             'SPACER_TWEET_HIGHLIGHT_OLD': 'dbdddf',
             
+            # Color of new highlighted tweets and spacers between them
+            # (if from the same user)
             'HIGHLIGHT': 'c5dced',
+            
+            # Color of old highlighted tweets and spacers between them
+            # (if from the same user)
             'HIGHLIGHT_OLD': 'e4e9ed',
+            
+            # Spacer between two new highlighted tweets from different users
             'SPACER_HIGHLIGHT': 'b7cbdf',
+            
+            # Spacer between two old highlighted tweets from different users
             'SPACER_HIGHLIGHT_OLD': 'c7d3df',
             
+            # Color of new messages(received) and spacers between them
             'MESSAGE': 'c8f2c8',
+            
+            # Color of old messages(received) and spacers between them
             'MESSAGE_OLD': 'dbf6db',
             
-            'MENTION': 'ffffab',
+            # Color of new mentions and spacers between them
+            # (if from the same user)
+            'MENTION': 'ffffab', 
+            
+            # Color of old mentions and spacers between them
+            # (if from the same user)
             'MENTION_OLD': 'ffffc2',
+            
+            # Spacer between new mentions from differen users
             'SPACER_MENTION': 'f7e97c',
+            
+            # Spacer between old mentions from different users
             'SPACER_MENTION_OLD': 'faf194',
             
+            # Spacer between a message that starts the new timeline and an 
+            # old tweet
+            'SPACER_MENTION_NEW': 'eadc75',
+            
+            # All links
             'LINKS': '004363',
+            
+            # Spacer above the 'more' link
             'LOADMORE': 'd2d2d2',
+            
+            # Color of the loading screen text
             'LOADING': '5b5b5b',
+            
+            # Color of time and retweet etc. infos
             'TIME': '555555'
         }
     }
