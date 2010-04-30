2c385f11e1a490db02197e45e11e2fe0e1d6eef0
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-04-30 21:56:49 +0200

    Rename u to e.

--- atarashii/usr/share/pyshared/atarashii/updater_tweet.py
+++ atarashii/usr/share/pyshared/atarashii/updater_tweet.py
@@ -102,15 +102,15 @@ class UpdaterTweet(object):
         # in so we need to request from mention.id - 1, instead of the last_id
         current_mention = self.last_mention_id
         self.last_mention_id = HTML_UNSET_ID
-        for u in updates:
-            if hasattr(u, 'is_mentioned') and u.is_mentioned \
-               and u.id > current_mention:
+        for e in updates:
+            if hasattr(e, 'is_mentioned') and e.is_mentioned \
+               and e.id > current_mention:
                 
-                self.last_mention_id = u.id - 1
+                self.last_mention_id = e.id - 1
                 
                 # and check if a reply exists
                 for i in updates:
-                    if not hasattr(i, 'is_mentioned') and i.id == u.id:
+                    if not hasattr(i, 'is_mentioned') and i.id == e.id:
                         self.last_mention_id = HTML_UNSET_ID
                         break
                 
