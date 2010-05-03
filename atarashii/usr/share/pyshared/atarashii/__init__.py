10fd790a4796787020c63d61183cac6084cc997c
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-05-03 19:49:39 +0200

    Update to Version 0.99.27 - Tōkei-Edition.

--- atarashii/usr/share/pyshared/atarashii/__init__.py
+++ atarashii/usr/share/pyshared/atarashii/__init__.py
@@ -16,7 +16,7 @@
 '''
 Atarashii Twitter Client
 '''
-__version__ = '0.99.26b'
+__version__ = '0.99.27'
 __author__ = 'Ivo Wetzel <ivo.wetzel@googlemail.com>'
 __copyright__ = 'Copyright (c) 2010 Ivo Wetzel'
 __license__ = 'GPL 3'
