28171426d7cca0dcd9db6f794f67965f7d8d389d
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-05-04 15:57:30 +0200

    Start the next bugfix release.

--- atarashii/usr/share/pyshared/atarashii/__init__.py
+++ atarashii/usr/share/pyshared/atarashii/__init__.py
@@ -16,7 +16,7 @@
 '''
 Atarashii Twitter Client
 '''
-__version__ = '0.99.27d'
+__version__ = '0.99.27e'
 __author__ = 'Ivo Wetzel <ivo.wetzel@googlemail.com>'
 __copyright__ = 'Copyright (c) 2010 Ivo Wetzel'
 __license__ = 'GPL 3'
